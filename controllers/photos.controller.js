f5755d627842922ae0642c727ffb15e07c119294
Waldemar Szymon <walldy@tlen.pl>
2023-08-22 06:45:25 +0200

    Add photo form validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,19 +1,41 @@
 const Photo = require('../models/photo.model');
+var sanitize = require('mongo-sanitize');
 
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
 
   try {
-    const { title, author, email } = req.fields;
+    const title = sanitize(req.fields.title);
+    const author = sanitize(req.fields.author);
+    const email = sanitize(req.fields.email);
     const file = req.files.file;
 
     if(title && author && email && file) { // if fields are not empty...
 
+      if (title.length > 25) {
+        throw new Error('Too long title');
+      }
+
+      if (author.length > 50) {
+        throw new Error('Too long author');
+      }
+
+      let regexEmail = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+      if (!regexEmail.test(email)) {
+        throw new Error('Wrong email');
+      }
+
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0].toLowerCase();
+      if (fileExt === 'jpg' || fileExt === 'gif' || fileExt === 'png') {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        throw new Error('Wrong file extension');
+      }
 
     } else {
       throw new Error('Wrong input!');
